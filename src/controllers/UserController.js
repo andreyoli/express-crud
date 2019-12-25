7154210c93a0a41bc7aef0ce80bde868b59d1e16
Andrey <andrey.o.paula@gmail.com>
2019-12-25 14:17:12 -0300

    Added index, destroy, find and update routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,5 +6,40 @@ module.exports = {
     const user = await User.create({ name, password, email })
 
     return res.send(`User ${user.name} has been registered`)
+  },
+
+  async index (req, res) {
+    const user = await User.findAll({ attributes: { exclude: ['password'] } })
+
+    return res.json(user)
+  },
+
+  async delete (req, res) {
+    const { email } = req.body
+
+    const user = await User.findOne({
+      where: { email },
+      attributes: { exclude: ['password'] }
+    })
+    await User.destroy({ where: { email } })
+
+    return res.send(`User ${user.name} delete`)
+  },
+
+  async show (req, res) {
+    const { id } = req.body
+    const user = await User.findOne({
+      where: { id },
+      attributes: { exclude: ['password'] }
+    })
+
+    return res.json(user)
+  },
+
+  async update (req, res) {
+    const { name, email } = req.body
+    const user = User.update({ name }, { where: { email } })
+
+    res.json(user)
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,9 @@ routes.get('/', (req, res) => {
 })
 
 routes.post('/user_register', UserController.store)
+routes.delete('/user_destroy', UserController.delete)
+routes.get('/user_index', UserController.index)
+routes.get('/user_find', UserController.show)
+routes.put('/user_update', UserController.update)
 
 module.exports = routes
